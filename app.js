b8d1fda47988c61152fca499bfbd972218fc0f8a
shariar <shariarmahmud225@gmail.com>
2024-07-14 17:22:20 +0600

    app.js modified

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 
+
+const app = express()
+
 app.use(express.json()) // ei middleware tar kaj hosche post method a data k jno json a convert kore felte apre 
 
 app.use(cors)
-
-const app = express()
 
 if (process.env.NODE_ENV === "development") {
     // amra jode application ta development a chalai tahole amra chache j amra ki ki request kokhn kortse ogula amader console.log a dekhak that's it 
