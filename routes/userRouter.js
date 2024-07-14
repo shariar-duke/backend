a67e8effb87bf91cd2db4f92feb457d41382a95b
shariar <shariarmahmud225@gmail.com>
2024-07-14 20:49:26 +0600

    signup , signin function imported from userController

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,6 +1,7 @@
 // ekhne amra user model er jnno necessary route gula define korbo 
 const express = require('express')
 const router = express.Router() // router k import korlam 
+const { signIn, signUp } = require('../controllers/userController')
 
 router.route('/signup').post(signUp)
 router.route('/signin').post(signIn)
