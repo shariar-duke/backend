64be9e41e3cd39967a139593597a2c218323375d
shariar <shariarmahmud225@gmail.com>
2024-07-14 20:34:20 +0600

    initial structure of userRouter

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,1 +1,19 @@
 // ekhne amra user model er jnno necessary route gula define korbo 
+const express = require('express')
+const router = express.Router() // router k import korlam 
+
+const signUp = () => {
+
+}
+
+const signIn = () => {
+
+}
+
+router.route('/signup').post(signUp)
+router.route('/signin').post(signIn)
+
+// A POST request to /signup will trigger the signUp function.
+// A POST request to /signin will trigger the signIn function.
+
+module.exports = router
