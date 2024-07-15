a8975ed6cf52e0e3f52d5b59a0100915072ee43e
shariar <shariarmahmud225@gmail.com>
2024-07-15 20:34:43 +0600

    category controller file initialized

---
+++ controllers/categoryController.js
@@ -0,0 +1,12 @@
+const _ = require('lodash')
+const { Category, validate } = require('../models/category')
+
+// ekhane duta function banabo ekta category create er jnno r ekta holo category get kroar jnno
+
+module.exports.createCategory = async (req, res) => {
+
+}
+
+module.exports.getCategories = async (req, res) => {
+
+}
