b5a95a6865519d57558304dcfe022d79c9c0885a
shariar <shariarmahmud225@gmail.com>
2024-07-15 20:34:13 +0600

    category router added on app.js

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 
-const userRouter = require("./routes/userRouter")  // user Router k import kore nia aslam 
+const userRouter = require("./routes/userRouter")
+const categoryRouter = require("./routes/categoryRouter")
 
 const app = express()
 
 app.use(express.json())
 
-app.use(cors()); // Call the middleware function
+app.use(cors());
 
 
 if (process.env.NODE_ENV === "development") {
@@ -18,16 +19,14 @@ if (process.env.NODE_ENV === "development") {
 }
 
 app.use('/api/user', userRouter)
+app.use('/api/category', categoryRouter)
 
-// ei middleware ta amra specially error handeing er jnno lekhtse jate promise handle kroete giey kno error paile seta handle korete pare 
-// kno middleware ta error handle korar jnno use korte hole er first pearmater ta err dite hbe 
+
 app.use((err, req, res, next) => {
     return res.status(500).send("Something Failed")
 })
 
 
-// last a ei middleware ta lekhlam karon er uporer jode route a promise type error hbe hole control ta pass hbe holo ei middlware a r eta porjnot seta pass kore dbe holo oi 
-// package ta jar nam holo express-async-errors , Ager middleware gular kno tate then use kora nai . so controll dhakka mare er kase dbe holo ei middleware ta 
 
 
 module.exports = app;
