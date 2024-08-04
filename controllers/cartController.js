7e2b808229b70ae55f314df4ab1ba2d386bea0b3
shariar <shariarmahmud225@gmail.com>
2024-08-04 14:05:22 +0600

    controller file created initially

---
+++ controllers/cartController.js
@@ -0,0 +1,24 @@
+const _ = require('lodash')
+const { CartItem } = require('../models/cartItem')
+
+// eta cartItem take create korbe 
+module.exports.createCartItem = async (req, res) => {
+
+}
+
+// eta cartItem gula k get korabe 
+module.exports.getCartItem = async (req, res) => {
+
+}
+
+// eta cartTIem gula k update korabe ...
+
+module.exports.updateCartItem = async (req, res) => {
+
+}
+
+// cart er kno item delete kore dite chaile eta korte hbe 
+
+module.exports.deleteCartItem = async (req, res) => {
+
+}
