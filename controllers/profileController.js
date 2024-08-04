566ef04747f4de94a38ff8f29f297e3d971fc7a5
shariar <shariarmahmud225@gmail.com>
2024-08-04 18:28:53 +0600

    profile controller created initally

---
+++ controllers/profileController.js
@@ -0,0 +1,14 @@
+const _ = require('lodash')
+const { Profile } = require("../models/profile")
+
+
+module.exports.getProfile = async (req, res) => {
+
+}
+
+
+// setProfile function ta diye ami eki sathe create o korbo update o korbo 
+
+module.exports.setProfile = async (req, res) => {
+    // jode profile thake na thake tahle cretae hbe r jode thake thake tahle seta update hbe 
+}
